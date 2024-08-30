e16541d5fde44df3d5938dfd0f1f9e583bca3270
s.zhukharev <s.zhukharev@vniias.ru>
2024-08-30 11:23:00 +0300

    add-delete-cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,39 @@
-// @todo: Темплейт карточки
+// Функция создания карточки
+function createCard(data, deleteCard) {
+  // Клонируем содержимое шаблона
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: DOM узлы
+  // Находим нужные элементы внутри карточки
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция создания карточки
+  // Устанавливаем значения для карточки
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
 
-// @todo: Функция удаления карточки
+  // Добавляем обработчик события на кнопку удаления
+  deleteButton.addEventListener('click', () => deleteCard(cardElement));
 
-// @todo: Вывести карточки на страницу
+  return cardElement;
+}
+
+// Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+// Выводим карточки на страницу
+function displayCards() {
+  const placesList = document.querySelector('.places__list');
+
+  initialCards.forEach(cardData => {
+    const cardElement = createCard(cardData, deleteCard);
+    placesList.append(cardElement);
+  });
+}
+
+// Вызов функции для отображения карточек при загрузке страницы
+displayCards();
